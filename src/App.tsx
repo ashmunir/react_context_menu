faf385752da28c8521c005bd4e9d37bbc4306e20
AyeshaMunir98 <ayesha.munir@ineffabledevs.com>
2022-06-28 23:00:53 +0500

    working with react-contextmenu

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+import ReactDOM from "react-dom";
+import { ContextMenu, MenuItem, ContextMenuTrigger } from "react-contextmenu";
+ 
+// function handleClick(e, data) {
+//   console.log(data.foo);
+// }
+ 
+function MyApp() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* NOTICE: id must be unique between EVERY <ContextMenuTrigger> and <ContextMenu> pair */}
+      {/* NOTICE: inside the pair, <ContextMenuTrigger> and <ContextMenu> must have the same id */}
+ 
+      <ContextMenuTrigger id="same_unique_identifier">
+        <div className="well">Right click to see the menu</div>
+      </ContextMenuTrigger>
+ 
+      <ContextMenu id="same_unique_identifier">
+        <MenuItem data={{foo: 'bar'}} onClick={handleClick}>
+          ContextMenu Item 1
+        </MenuItem>
+        <MenuItem data={{foo: 'bar'}} onClick={handleClick}>
+          ContextMenu Item 2
+        </MenuItem>
+        <MenuItem divider />
+        <MenuItem data={{foo: 'bar'}} onClick={handleClick}>
+          ContextMenu Item 3
+        </MenuItem>
+      </ContextMenu>
+ 
     </div>
   );
 }
